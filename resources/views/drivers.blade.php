212a578c626f7a6f3fb3fcb7a2d1129af2e50cb9
Chathurika Weerasena <chathuweerasena@gmail.com>
2024-09-24 10:24:02 +0200

    feat:initiate basic navigation bar for three main pages

---
+++ resources/views/drivers.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+       Drivers
+    </x-slot:heading> 
+<h1> F1 Drivers</h1> 
+</x-layout>
---
+++ resources/views/schedule.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+        F1 Calendar
+    </x-slot:heading> 
+<h1> F1 Calendar</h1> 
+</x-layout>
